f44ce5b491568f9f609738503b37067b2c0b43ff
LIULE9 <LEO.ZHEN.CONG.LIU@OOCL.COM>
2018-11-13 11:45:29 +0800

    Spring scurity 尝试自定义请求模式

--- src/main/java/com/leo/springsecuritydemo/config/WebSecurityConfig.java
+++ src/main/java/com/leo/springsecuritydemo/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.leo.springsecuritydemo.config;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,12 +12,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * @create 2018-11-13 10:38 AM
  */
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
+        .antMatchers("/resources/**", "/signup", "/about").permitAll()
+        .antMatchers(HttpMethod.OPTIONS).permitAll()
+        .antMatchers(HttpMethod.POST, "/employees", "/orders").permitAll()
+        .antMatchers("/admin/**").hasRole("ADMIN")
+        .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+        .antMatchers("/hello").hasAuthority("ADD")
         .anyRequest().authenticated()
         .and()
         .formLogin()
